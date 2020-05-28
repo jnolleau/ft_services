9bc94146dbf260987ac1ca0f6e462b00c2a04845
Sequoiageant <j.nolleau88@gmail.com>
2020-05-28 19:16:29 +0200

    1 dashboard/container almost done

--- srcs/telegraf/launch.sh
+++ srcs/telegraf/launch.sh
@@ -1,7 +1,7 @@
 docker build -t telegraf_local .
 docker run --name telegraf --rm -ti \
 			-e USER \
-			-e MINIKUBE_IP=172.17.0.2 \
+			-e MINIKUBE_IP=172.17.0.3 \
 			-v /var/run/docker.sock:/var/run/docker.sock:ro \
 			--net=influxdb \
 			-p 4999:4999 telegraf_local
--- srcs/telegraf/telegraf.sh
+++ srcs/telegraf/telegraf.sh
@@ -6,7 +6,3 @@ if [ -e /var/run/docker.sock ]
 		chown root:root /var/run/docker.sock
 fi
 cd /telegraf/usr/bin && ./telegraf --config "/telegraf/etc/telegraf/telegraf.conf"
-# openrc default
-# rc-update add telegraf default
-# rc-service telegraf start 2> /dev/NULL
-# /bin/sh
